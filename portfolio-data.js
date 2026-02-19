46def914bed2af57fc349a5a8b5727636051ab4b
PraviWealth <abhisidd.4u@gmail.com>
2026-02-19 20:32:17 +0530

    Add files via upload

---
+++ portfolio-data.js
@@ -0,0 +1,16 @@
+// Portfolio Data - Last Updated: 2/19/2026, 8:29:00 PM
+const PORTFOLIO_DATA = [
+  {
+    "id": 1771513097799,
+    "symbol": "RELIANCE",
+    "name": "Reliance Industries Ltd",
+    "exchange": "NSE",
+    "quantity": 100,
+    "entryPrice": 150,
+    "entryDate": "2026-02-19",
+    "exitPrice": null,
+    "exitDate": null,
+    "notes": "Sl is 140",
+    "addedDate": "2026-02-19T14:58:17.799Z"
+  }
+];
