7ec932737aaa246aecb619271c9dd9521f0681b1
PraviWealth <abhisidd.4u@gmail.com>
2026-02-19 20:38:01 +0530

    Add files via upload

---
+++ portfolio-data.js
@@ -0,0 +1,16 @@
+// Portfolio Data - Last Updated: 2/19/2026, 8:36:46 PM
+const PORTFOLIO_DATA = [
+  {
+    "id": 1771513594671,
+    "symbol": "APEX",
+    "name": "APEX",
+    "exchange": "NSE",
+    "quantity": 100,
+    "entryPrice": 160,
+    "entryDate": "2026-02-19",
+    "exitPrice": null,
+    "exitDate": null,
+    "notes": "Sl is 140",
+    "addedDate": "2026-02-19T15:06:34.671Z"
+  }
+];
